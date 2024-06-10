a6cc1d3a85694a0ec9a15530aca74363549dc556
Jade Smith <jade@jade-smith.com>
2024-06-09 20:32:25 -0500

    indenting change

--- nix/home/home.nix
+++ nix/home/home.nix
@@ -75,29 +75,27 @@ in
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
-  {
-    wayland.windowManager.hyprland = {
-      enable = true;
+  wayland.windowManager.hyprland = {
+    enable = true;
 
-      plugins = [
-        inputs.hyprland-plugins.packages."${pkgs.system}".borders-plus-plus
-      ];
+    plugins = [
+      inputs.hyprland-plugins.packages."${pkgs.system}".borders-plus-plus
+    ];
 
-      settings = {
-        "plugin:borders-plus-plus" = {
-          add_borders = 1;
+    settings = {
+      "plugin:borders-plus-plus" = {
+        add_borders = 1;
 
-          "col.border_1" = "rgb(ffffff)";
-          "col.border_2" = "rgb(2222ff)";
+        "col.border_1" = "rgb(ffffff)";
+        "col.border_2" = "rgb(2222ff)";
 
-          border_size_1 = 10;
-          border_size_2 = -1;
+        border_size_1 = 10;
+        border_size_2 = -1;
 
-          natural_rounding = "yes";
-        };
+        natural_rounding = "yes";
       };
     };
-  }
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
