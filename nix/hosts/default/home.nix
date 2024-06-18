8d2e4d4e198e8d8e0ff3184745db6758bb989b55
Jade Smith <jade@jade-smith.com>
2024-06-17 19:28:03 -0500

    adding hyprland settings

--- nix/hosts/default/home.nix
+++ nix/hosts/default/home.nix
@@ -17,4 +17,15 @@ in {
   home.sessionVariables = { };
 
   programs.home-manager.enable = true;
+
+  wayland.windowManager.hyprland.settings = {
+    "$mod" = "SUPER";
+    bind = [ "$mod, F, exec, firefox" ", Print, exec, grimblast copy area" ]
+      ++ (builtins.concatLists (builtins.genList (x:
+        let ws = let c = (x + 1) / 10; in builtins.toString (x + 1 - (c * 10));
+        in [
+          "$mod, ${ws}, workspace, ${toString (x + 1)}"
+          "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+        ]) 10));
+  };
 }
