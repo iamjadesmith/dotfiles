9a31dd20e493f9efe9bce857f473b24b5f80309a
Jade Smith <jade@jade-smith.com>
2024-06-17 19:21:26 -0500

    fixing hyprland

--- nix/hosts/default/configuration.nix
+++ nix/hosts/default/configuration.nix
@@ -34,7 +34,7 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  wayland.windowManager.hyprland.enable = true;
+  programs.hyprland.enable = true;
 
   services.xserver.xkb = {
     layout = "us";
