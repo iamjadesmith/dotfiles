a5c5f3c952a7ab71893601fb859480ad033c0ae6
Jade Smith <jade@jade-smith.com>
2024-06-21 19:31:07 -0500

    xwayland setting

--- nix/hosts/default/home.nix
+++ nix/hosts/default/home.nix
@@ -65,5 +65,6 @@ in
       "gsettings set org.gnome.desktop.interface gtk-theme ""Adwaita"""
       "gsettings set org.gnome.desktop.interface color-scheme ""prefer-dark"""
     ];
+    xwayland.force_zero_scaling = true;
   };
 }
