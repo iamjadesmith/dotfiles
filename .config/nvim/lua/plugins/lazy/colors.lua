2cff9582032c07760613fec7549d073514c9977c
Jade Smith <jade@jade-smith.com>
2024-07-20 13:48:35 -0500

    dark mode

--- .config/nvim/lua/plugins/lazy/colors.lua
+++ .config/nvim/lua/plugins/lazy/colors.lua
@@ -5,8 +5,8 @@ return {
 		priority = 1000,
 		opts = {},
 		config = function()
-			-- vim.cmd([[colorscheme tokyonight-night]])
-			vim.cmd([[colorscheme tokyonight-day]])
+			vim.cmd([[colorscheme tokyonight-night]])
+			-- vim.cmd([[colorscheme tokyonight-day]])
 		end,
 	},
 }
