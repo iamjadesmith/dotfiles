ee5545570025e859ffe953b234a61167ff2e869e
Jade Smith <jade@jade-smith.com>
2024-09-29 17:50:36 -0500

    large config changes and adding ags

---
+++ nix/hosts/default/alacritty.nix
@@ -0,0 +1,53 @@
+{
+  pkgs,
+  meta,
+  ...
+}: {
+  enable = true;
+  package = pkgs.unstable.alacritty;
+
+  settings = {
+    window = {
+      padding = {
+        x = 4;
+        y = 8;
+      };
+      decorations = "full";
+      opacity = 1;
+      startup_mode = "Windowed";
+      title = "Alacritty";
+      dynamic_title = true;
+      decorations_theme_variant = "None";
+    };
+
+    import = [
+      pkgs.alacritty-theme.tokyo-night
+    ];
+
+    font = let
+      jetbrainsMono = style: {
+        family = "JetBrainsMono Nerd Font";
+        inherit style;
+      };
+    in {
+      size =
+        if meta.name == "karasu"
+        then 12
+        else 16;
+      normal = jetbrainsMono "Regular";
+      bold = jetbrainsMono "Bold";
+      italic = jetbrainsMono "Italic";
+      bold_italic = jetbrainsMono "Bold Italic";
+    };
+
+    cursor = {
+      style = "Block";
+    };
+
+    env = {
+      TERM = "xterm-256color";
+    };
+
+    live_config_reload = true;
+  };
+}
