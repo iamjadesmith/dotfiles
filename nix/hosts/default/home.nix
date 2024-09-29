5b10db92c4d64ee20923d34ab16c7a27e7b646b6
Jade Smith <jade@jade-smith.com>
2024-09-29 17:50:36 -0500

    large config changes and adding ags

--- nix/hosts/default/home.nix
+++ nix/hosts/default/home.nix
@@ -1,9 +1,12 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, meta, ... }:
 let
   inherit (config.lib.file) mkOutOfStoreSymlink;
-  mod = "Mod4";
 in
 {
+  imports = [
+    ./ags/default.nix
+  ];
+
   home.username = "joejad";
   home.homeDirectory = "/home/joejad";
   xdg.enable = true;
@@ -20,78 +23,25 @@ in
 
   programs.home-manager.enable = true;
 
-  wayland.windowManager.hyprland.enable = true;
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    bind =
-      [
-        "$mod, F, exec, firefox"
-        "$mod, RETURN, exec, alacritty"
-        "$mod+SHIFT, E, exit"
-        "$mod+SHIFT, Q, killactive"
-        "$mod, Y, exec, nautilus"
-        "$mod, V, togglesplit"
-        "$mod, H, movefocus, l"
-        "$mod, L, movefocus, r"
-        "$mod, K, movefocus, u"
-        "$mod, J, movefocus, d"
-        "$mod+SHIFT, H, movewindow, l"
-        "$mod+SHIFT, L, movewindow, r"
-        "$mod+SHIFT, K, movewindow, u"
-        "$mod+SHIFT, J, movewindow, d"
-        "$mod, D, exec, wofi --show drun"
-        "$mod+ALT, H, resizeactive, -10 -10"
-        "$mod+ALT, L, resizeactive, 10 -10"
-        "$mod+ALT, K, resizeactive, 10 -10"
-        "$mod+ALT, J, resizeactive, -10 -10"
-      ]
-      ++ (builtins.concatLists (
-        builtins.genList (
-          x:
-          let
-            ws =
-              let
-                c = (x + 1) / 10;
-              in
-              builtins.toString (x + 1 - (c * 10));
-          in
-          [
-            "$mod, ${ws}, workspace, ${toString (x + 1)}"
-            "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-          ]
-        ) 10
-      ));
-    monitor = [
-      "DP-3,3440x1440@175,1920x0,1"
-      "Unknown-1,disable"
-    ];
-    exec = [
-      ''gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"   # for GTK3 apps''
-      ''gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"   # for GTK4 apps''
-    ];
-    xwayland.force_zero_scaling = true;
-  };
-
-  xsession.windowManager.i3 = {
+  gtk = {
     enable = true;
-    package = pkgs.i3-gaps;
-    config = {
-      modifier = mod;
-
-      keybindings = lib.mkOptionDefault {
-        "${mod}+h" = "focus left";
-        "${mod}+j" = "focus down";
-        "${mod}+k" = "focus up";
-        "${mod}+l" = "focus right";
-
-        "${mod}+Shift+h" = "move left";
-        "${mod}+Shift+j" = "move down";
-        "${mod}+Shift+k" = "move up";
-        "${mod}+Shift+l" = "move right";
-
-        "${mod}+Return" = "exec alacritty";
-      };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
     };
   };
+
+  dconf = {
+    enable = true;
+    settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+  };
+
+  programs = {
+    alacritty = import ./alacritty.nix {inherit config pkgs meta;};
+  };
+
+  wayland.windowManager = {
+    hyprland = import ./hyprland.nix {inherit pkgs meta config;};
+  };
+
 
 }
---
+++ nix/hosts/default/hyprland.nix
@@ -0,0 +1,53 @@
+{
+  enable = true;
+  settings = {
+    "$mod" = "SUPER";
+    bind =
+      [
+        "$mod, F, exec, firefox"
+        "$mod, RETURN, exec, alacritty"
+        "$mod+SHIFT, E, exit"
+        "$mod+SHIFT, Q, killactive"
+        "$mod, Y, exec, nautilus"
+        "$mod, V, togglesplit"
+        "$mod, H, movefocus, l"
+        "$mod, L, movefocus, r"
+        "$mod, K, movefocus, u"
+        "$mod, J, movefocus, d"
+        "$mod+SHIFT, H, movewindow, l"
+        "$mod+SHIFT, L, movewindow, r"
+        "$mod+SHIFT, K, movewindow, u"
+        "$mod+SHIFT, J, movewindow, d"
+        "$mod, D, exec, wofi --show drun"
+        "$mod+ALT, H, resizeactive, -10 -10"
+        "$mod+ALT, L, resizeactive, 10 -10"
+        "$mod+ALT, K, resizeactive, 10 -10"
+        "$mod+ALT, J, resizeactive, -10 -10"
+      ]
+      ++ (builtins.concatLists (
+        builtins.genList (
+          x:
+          let
+            ws =
+              let
+                c = (x + 1) / 10;
+              in
+              builtins.toString (x + 1 - (c * 10));
+          in
+          [
+            "$mod, ${ws}, workspace, ${toString (x + 1)}"
+            "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+          ]
+        ) 10
+      ));
+    monitor = [
+      "DP-3,3440x1440@175,1920x0,1"
+      "Unknown-1,disable"
+    ];
+    exec = [
+      ''gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"   # for GTK3 apps''
+      ''gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"   # for GTK4 apps''
+    ];
+    xwayland.force_zero_scaling = true;
+  };
+}
