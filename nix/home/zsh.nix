ec9388a1f734fe3974a0fde44d5ac2d23031d4fd
Jade Smith <jade@jade-smith.com>
2024-10-31 17:32:30 -0500

    adding zsh

--- nix/home/zsh.nix
+++ nix/home/zsh.nix
@@ -16,7 +16,7 @@
       eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
 
-    ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+    ZINIT_HOME="${XDG_DATA_HOME:${HOME}/.local/share}/zinit/zinit.git"
     if [ ! -d "$ZINIT_HOME" ]; then
        mkdir -p "$(dirname $ZINIT_HOME)"
        git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
