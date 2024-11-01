0d8d80914ab8480553fd94507ee3ceec75a6d923
Jade Smith <jade@jade-smith.com>
2024-10-31 20:20:13 -0500

    adding fzf

--- nix/home/zsh.nix
+++ nix/home/zsh.nix
@@ -16,7 +16,7 @@
       eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
 
-    ZINIT_HOME="~/.local/share/zinit/zinit.git"
+    ZINIT_HOME="$HOME/.local/share/zinit/zinit.git"
     if [ ! -d "$ZINIT_HOME" ]; then
        mkdir -p "$(dirname $ZINIT_HOME)"
        git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
@@ -57,7 +57,6 @@
     setopt hist_find_no_dups
 
     zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-    zstyle ':completion:*' list-colors "$(s.:.)LS_COLORS"
     zstyle ':completion:*' menu no
     zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
     zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
