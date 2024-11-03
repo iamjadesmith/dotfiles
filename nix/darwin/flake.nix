df6e3586f0de51b30db5be4571802ea68ae18ca0
Jade Smith <jade@Jades-MacBook-Pro.local>
2024-11-03 11:23:35 -0600

    darwin config changes

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -5,16 +5,78 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, ... }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.vim
+        [
+	  pkgs.alacritty
+          pkgs.git
+          pkgs.unstable.go_1_23
+          pkgs.lua-language-server
+          pkgs.mkalias
+          pkgs.neovim
+          pkgs.nil
+          pkgs.obsidian
+          pkgs.ripgrep
+          pkgs.stylua
+          pkgs.tmux
+          pkgs.zoxide
         ];
+
+      users.users.jade = {
+        name = username;
+        home = "/Users/jade";
+      };
+
+      homebrew = {
+        enable = true;
+        brews = [
+          "mas"
+        ];
+        casks = [
+          "alfred"
+          "firefox"
+          "discord"
+        ];
+        taps = [
+        ];
+	masApps = {
+	};
+      };
+
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+          # Set up applications.
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
+
+      system.defaults = {
+        NSGlobalDomain.AppleShowAllExtensions = true;
+        loginwindow.GuestEnabled = false;
+        finder.FXPreferredViewStyle = "clmv";
+      };
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
@@ -42,7 +104,16 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#mac
     darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [
+        configuration
+        home-manager.darwinModules.home-manager
+        {
+          # `home-manager` config
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.jade = import ./home.nix;
+        }
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
