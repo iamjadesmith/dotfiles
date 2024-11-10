562c9d38dcb79b9735390c657d2d6bc051b45813
Jade Smith <jade@jade-smith.com>
2024-11-09 19:21:48 -0600

    open drivers

--- nix/hosts/default/configuration.nix
+++ nix/hosts/default/configuration.nix
@@ -149,17 +149,14 @@
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = true;
 
-  hardware.graphics = {
-    enable = true;
-    # enable32Bit = true;
-  };
+  hardware.graphics.enable = true;
 
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
     powerManagement.finegrained = false;
-    open = false;
+    open = true;
     nvidiaSettings = true;
     # package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
