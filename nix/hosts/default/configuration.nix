b8c37a411e3b439f6fc4de34d6173d3fe3b17df6
Jade Smith <jade@jade-smith.com>
2024-11-09 19:45:16 -0600

    fixed nvidia garbage

--- nix/hosts/default/configuration.nix
+++ nix/hosts/default/configuration.nix
@@ -153,11 +153,9 @@
 
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
-    # modesetting.enable = true;
     powerManagement.enable = true;
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 }
