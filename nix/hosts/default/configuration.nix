1848abf4055b34c7ebfb3a86755e76dbfb55bc6f
Jade Smith <jade@jade-smith.com>
2024-11-09 20:24:09 -0600

    555 driver

--- nix/hosts/default/configuration.nix
+++ nix/hosts/default/configuration.nix
@@ -165,7 +165,7 @@
       sha256_aarch64 = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
       openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
       settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
-      # persistencedSha256 = lib.fakeSha256;
+      persistencedSha256 = lib.fakeSha256;
     };
   };
 }
