04fccfbed65033af522d965bbdf1e532a5161d0e
Jade Smith <jade@jade-smith.com>
2024-11-16 09:26:07 -0600

    zen browser keybind to hyprland

--- nix/home/hyprland.nix
+++ nix/home/hyprland.nix
@@ -7,7 +7,7 @@ in
     "$mod" = "ALT";
     bind =
       [
-        "$mod, F, exec, firefox"
+        "$mod, F, exec, zen"
         "$mod, RETURN, exec, alacritty"
         "$mod+SUPER, E, exit"
         "$mod+SUPER, S, exec, systemctl suspend"
