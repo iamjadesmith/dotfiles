5936a77b05d603adbf7a79660e7fd611fdf1f416
Jade Smith <jade@jade-smith.com>
2024-12-01 16:51:30 -0600

    removing fugitive and adding dotfiles keymaps

--- .config/nvim/lua/plugins/remap.lua
+++ .config/nvim/lua/plugins/remap.lua
@@ -32,9 +32,11 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>")
+vim.keymap.set("n", "<leader>pn", "<cmd>e ~/.dotfiles/.config/nvim/lua/plugins<CR>", { desc = "Nvim Config" })
+vim.keymap.set("n", "<leader>pd", "<cmd>e ~/.dotfiles<CR>", { desc = "Dotfiles Config" })
+
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
 
 vim.keymap.set("n", "<leader><leader>", function()
-  vim.cmd("so")
+	vim.cmd("so")
 end)
