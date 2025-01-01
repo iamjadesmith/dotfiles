ac9dceb26c372ca4336063c95dae7de3dbaa5865
Jade Smith <jade@jade-smith.com>
2025-01-01 17:36:13 -0500

    remove swift things

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -1,32 +1,36 @@
-vim.opt.runtimepath:append("$HOME/.local/share/treesitter")
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	require("nvim-treesitter.configs").setup({
-    parser_install_dir = "$HOME/.local/share/treesitter",
-		ensure_installed = {
-			"vimdoc",
-			"javascript",
-			"typescript",
-			"markdown",
-			"markdown_inline",
-			"query",
-			"c",
-			"lua",
-			"rust",
-			"jsdoc",
-			"bash",
-			"r",
-			"python",
-			"swift",
-		},
-
-		sync_install = false,
-		auto_install = true,
-
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-	}),
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"markdown",
+					"markdown_inline",
+					"rust",
+					"r",
+					"python",
+					"bash",
+					"nix",
+				},
+				auto_install = false,
+				highlight = {
+					enable = true,
+					disable = function(lang, buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							return true
+						end
+					end,
+					additional_vim_regex_highlighting = false,
+				},
+			})
+		end,
+	},
 }
