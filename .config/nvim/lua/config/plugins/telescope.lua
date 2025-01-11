d0d688b02b7de53ce11faf015f80a6d6e3878a8b
Jade Smith <jade@jade-smith.com>
2025-01-11 08:56:10 -0600

    fzf telescope native plugin

--- .config/nvim/lua/config/plugins/telescope.lua
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -3,10 +3,17 @@ return {
 	tag = "0.1.8",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
 
 	config = function()
-		require("telescope").setup({})
+		require("telescope").setup({
+			extensions = {
+				fzf = {},
+			},
+		})
+
+		require("telescope").load_extension("fzf")
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
