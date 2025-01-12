baeb3f5da3d054ca0336287aa45fb0ba2ab49fa1
Jade Smith <jade@jade-smith.com>
2025-01-12 10:28:40 -0600

    fix for minecraft flickering

--- nix/home/hyprland.nix
+++ nix/home/hyprland.nix
@@ -72,13 +72,14 @@ in
     ];
   };
   extraConfig = ''
-          device {
-            name=logitech-g502-1
-            sensitivity=-1.0
-          }
-          device {
-            name=logitech-g502
-            sensitivity=-1.0
-          }
-    		'';
+              device {
+                name=logitech-g502-1
+                sensitivity=-1.0
+              }
+              device {
+                name=logitech-g502
+                sensitivity=-1.0
+              }
+    					render:explicit_sync = 0
+        		'';
 }
