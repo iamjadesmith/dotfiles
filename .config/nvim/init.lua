c652d8d41a0055374adab3cfca3aeaac6e4bb515
Jade Smith <jade@jade-smith.com>
2025-02-01 07:08:46 -0600

    no word wrapping

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,11 +4,11 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.wrap = false
 -- vim.opt.softtabstop = 2
 -- vim.opt.expandtab = true
 
 -- vim.opt.smartindent = true
--- vim.opt.wrap = false
 -- vim.opt.swapfile = false
 -- vim.opt.backup = false
 -- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
