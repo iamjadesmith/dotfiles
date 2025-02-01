b39aa3dc84ff2fca755194e2500480fdb7b16644
Jade Smith <jade@jade-smith.com>
2025-02-01 08:08:29 -0600

    set home-manager to nixpkgs

--- nix/flake.nix
+++ nix/flake.nix
@@ -6,7 +6,7 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
