3eb4cd276149a17d5b7f8a61baa4b64e177677d8
Jade Smith <jade@jade-smith.com>
2025-02-01 07:07:44 -0600

    server and desktop .dotfiles together

---
+++ nix/home/home-server.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  pkgs,
+  lib,
+  meta,
+  ...
+}:
+let
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+in
+{
+  home.username = "joejad";
+  home.homeDirectory = "/home/joejad";
+  xdg.enable = true;
+
+  xdg.configFile.nvim.source = mkOutOfStoreSymlink "/home/joejad/.dotfiles/.config/nvim";
+
+  home.stateVersion = "24.05";
+
+  home.packages = [ ];
+
+  home.file = { };
+
+  home.sessionVariables = { };
+
+  programs.home-manager.enable = true;
+
+  programs = {
+    zsh = import ./zsh.nix { inherit config; };
+    fzf = import ./fzf.nix { inherit pkgs; };
+    neovim = import ./neovim.nix { inherit config pkgs; };
+    tmux = import ./tmux.nix { inherit pkgs; };
+    git = import ./git.nix { inherit config pkgs; };
+    zoxide = import ./zoxide.nix { inherit pkgs; };
+  };
+
+}
