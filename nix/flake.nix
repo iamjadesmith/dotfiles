a07b651cd991d7f1e2dc6bae6f92054a8749a92f
Jade Smith <jade@jade-smith.com>
2025-02-01 08:23:46 -0600

    change home-manager release

--- nix/flake.nix
+++ nix/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     disko.url = "github:nix-community/disko";
