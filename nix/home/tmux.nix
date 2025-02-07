fe2b2cd4760c22af27c7e975526312020d56d93a
joejad <bill@joejad.com>
2025-02-07 10:21:24 -0600

    change status bar color

--- nix/home/tmux.nix
+++ nix/home/tmux.nix
@@ -18,46 +18,49 @@
   ];
 
   extraConfig = ''
-    # set-default colorset-option -ga terminal-overrides ",xterm-256color:Tc"
-    set -as terminal-features ",xterm-256color:RGB"
-    # set-option -sa terminal-overrides ",xterm*:Tc"
-    set -g mouse on
+        # set-default colorset-option -ga terminal-overrides ",xterm-256color:Tc"
+        set -as terminal-features ",xterm-256color:RGB"
+        # set-option -sa terminal-overrides ",xterm*:Tc"
+        set -g mouse on
 
-    bind -r ^ last-window
-    bind -r k select-pane -U
-    bind -r j select-pane -D
-    bind -r h select-pane -L
-    bind -r l select-pane -R
+        bind -r ^ last-window
+        bind -r k select-pane -U
+        bind -r j select-pane -D
+        bind -r h select-pane -L
+        bind -r l select-pane -R
 
-    set -g base-index 1
-    set -g pane-base-index 1
-    set-window-option -g pane-base-index 1
-    set-option -g renumber-windows on
+        set -g base-index 1
+        set -g pane-base-index 1
+        set-window-option -g pane-base-index 1
+        set-option -g renumber-windows on
 
-    # Bind clearing the screen
-    bind L send-keys '^L'
+        # Bind clearing the screen
+        bind L send-keys '^L'
 
-    # Shift arrow to switch windows
-    bind -n S-Left  previous-window
-    bind -n S-Right next-window
+    		set -g status-bg black
+    		set -g status-fg white
 
-    # Shift Alt vim keys to switch windows
-    bind -n M-H previous-window
-    bind -n M-L next-window
+        # Shift arrow to switch windows
+        bind -n S-Left  previous-window
+        bind -n S-Right next-window
 
-    # set vi-mode
-    set-window-option -g mode-keys vi
+        # Shift Alt vim keys to switch windows
+        bind -n M-H previous-window
+        bind -n M-L next-window
 
-    # keybindings
-    bind-key -T copy-mode-vi v send-keys -X begin-selection
-    bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-    bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+        # set vi-mode
+        set-window-option -g mode-keys vi
 
-    bind '"' split-window -v -c "#{pane_current_path}"
-    bind % split-window -h -c "#{pane_current_path}"
-    bind c new-window -c "#{pane_current_path}"
+        # keybindings
+        bind-key -T copy-mode-vi v send-keys -X begin-selection
+        bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+        bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
 
-    set -gu default-command
-    set -g default-shell "$SHELL"
+        bind '"' split-window -v -c "#{pane_current_path}"
+        bind % split-window -h -c "#{pane_current_path}"
+        bind c new-window -c "#{pane_current_path}"
+
+        set -gu default-command
+        set -g default-shell "$SHELL"
   '';
 }
