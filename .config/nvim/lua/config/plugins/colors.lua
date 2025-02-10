7223861c377b606ff864b8c8f334aeb5e48d3353
joejad <bill@joejad.com>
2025-02-10 09:35:23 -0600

    changed colors

---
+++ .config/nvim/lua/config/plugins/colors.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("tokyonight-night")
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		-- config = function()
+		-- 	vim.cmd.colorscheme("catppuccin-latte")
+		-- end,
+	},
+}
