2657d7440fddb058d0c6a37e2156177adcd4563b
joejad <bill@joejad.com>
2025-02-11 09:20:52 -0600

    add back some neovim options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,14 +5,9 @@ vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.wrap = false
--- vim.opt.softtabstop = 2
--- vim.opt.expandtab = true
-
--- vim.opt.smartindent = true
--- vim.opt.swapfile = false
--- vim.opt.backup = false
--- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
--- vim.opt.undofile = true
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -22,7 +17,7 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
--- vim.opt.updatetime = 50
+vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.api.nvim_create_autocmd("TextYankPost", {
