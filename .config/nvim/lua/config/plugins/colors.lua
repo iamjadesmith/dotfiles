fe08d18131ea831dfc9edc5eb1b7ff9dea2e391f
joejad <bill@joejad.com>
2025-02-12 07:36:41 -0600

    dark mode variable

--- .config/nvim/lua/config/plugins/colors.lua
+++ .config/nvim/lua/config/plugins/colors.lua
@@ -1,18 +1,25 @@
+local dark_mode = true
+
 return {
 	{
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("tokyonight-night")
+			if dark_mode then
+				vim.cmd.colorscheme("tokyonight-night")
+			end
 		end,
 	},
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
+		lazy = false,
 		priority = 1000,
-		-- config = function()
-		-- 	vim.cmd.colorscheme("catppuccin-latte")
-		-- end,
+		config = function()
+			if not dark_mode then
+				vim.cmd.colorscheme("catppuccin-latte")
+			end
+		end,
 	},
 }
