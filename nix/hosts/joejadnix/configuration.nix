150c3658fc2fa21d1e94f38ee76f53fca21b48d8
Jade Smith <jade@jade-smith.com>
2025-02-23 13:57:30 -0600

    change pull policy

--- nix/hosts/joejadnix/configuration.nix
+++ nix/hosts/joejadnix/configuration.nix
@@ -69,6 +69,7 @@
     ollama = {
       image = "ollama/ollama";
       ports = [ "11434:11434" ];
+      pull = "always";
       volumes = [
         "/var/lib/ollama:/root/.ollama"
       ];
@@ -80,6 +81,7 @@
     open-webui = {
       image = "ghcr.io/open-webui/open-webui:main";
       ports = [ "3000:8080" ];
+      pull = "always";
       volumes = [
         "/var/lib/open-webui:/app/backend/data"
       ];
