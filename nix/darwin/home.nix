e920bfa1757e8b499e5026c86345d3ae9f316cae
Jade Smith <jade@jade-smith.com>
2025-03-05 17:09:42 -0600

    syncthing as service

--- nix/darwin/home.nix
+++ nix/darwin/home.nix
@@ -4,9 +4,11 @@
   lib,
   meta,
   ...
-}: let
+}:
+let
   inherit (config.lib.file) mkOutOfStoreSymlink;
-in {
+in
+{
   programs.home-manager.enable = true;
 
   home.username = "jade";
@@ -14,16 +16,30 @@ in {
   xdg.enable = true;
 
   xdg.configFile.nvim.source = mkOutOfStoreSymlink "/Users/jade/.dotfiles/.config/nvim";
-    #xdg.configFile.aerospace.source = mkOutOfStoreSymlink "/Users/jade/.dotfiles/.config/aerospace";
+  #xdg.configFile.aerospace.source = mkOutOfStoreSymlink "/Users/jade/.dotfiles/.config/aerospace";
 
   home.stateVersion = "24.05";
 
+  services.syncthing = {
+    enable = true;
+    group = "staff";
+    user = "jade";
+    configDir = "/Users/joejad/.config/syncthing";
+  };
+
   programs = {
-    alacritty = import ../home/alacritty.nix { inherit lib config pkgs meta; };
-    tmux = import ../home/tmux.nix {inherit pkgs;};
-    zsh = import ../home/zsh.nix {inherit config pkgs lib; };
+    alacritty = import ../home/alacritty.nix {
+      inherit
+        lib
+        config
+        pkgs
+        meta
+        ;
+    };
+    tmux = import ../home/tmux.nix { inherit pkgs; };
+    zsh = import ../home/zsh.nix { inherit config pkgs lib; };
     zoxide = (import ../home/zoxide.nix { inherit config pkgs; });
-    fzf = import ../home/fzf.nix {inherit pkgs;};
-    git = import ../home/git.nix {inherit config pkgs;};
+    fzf = import ../home/fzf.nix { inherit pkgs; };
+    git = import ../home/git.nix { inherit config pkgs; };
   };
 }
