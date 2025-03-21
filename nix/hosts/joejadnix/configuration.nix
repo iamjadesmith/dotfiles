2e5ecaf8b302c374c5ee39b9cc933d2f54ee1f60
Jade Smith <jade@jade-smith.com>
2025-03-21 12:35:35 -0500

    systemd workout

--- nix/hosts/joejadnix/configuration.nix
+++ nix/hosts/joejadnix/configuration.nix
@@ -254,6 +254,18 @@
     open = true;
   };
 
+  systemd.services.workout = {
+    description = "Workout";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "/home/joejad/projects/workout_python/workout.sh";
+      User = "joejad";
+      Group = "users";
+      Restart = "on-failure";
+    };
+  };
+
   hardware.nvidia-container-toolkit.enable = true;
 
   networking.firewall = {
