6f3fd59051036ebe732bd73fc927b5c81b3ce970
Jade Smith <jade@jade-smith.com>
2025-03-22 12:57:07 -0500

    hyprcursor

--- nix/home/home-light.nix
+++ nix/home/home-light.nix
@@ -33,8 +33,6 @@ in
 
   home.pointerCursor = {
     gtk.enable = true;
-    # package = pkgs.rose-pine-hyprcursor;
-    # name = "rose-pine-hyprcursor";
     package = pkgs.rose-pine-cursor;
     name = "BreezeX-RosePine-Linux";
     size = 24;
