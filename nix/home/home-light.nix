b34a59797e42c9708f6ee92993b16f36d1a47403
Jade Smith <jade@jade-smith.com>
2025-03-22 12:04:55 -0500

    hyprcursor

--- nix/home/home-light.nix
+++ nix/home/home-light.nix
@@ -29,6 +29,15 @@ in
 
   gtk = {
     enable = true;
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+
+    iconTheme = {
+      package = pkgs.gnome.adwaita-icon-theme;
+      name = "Adwaita";
+    };
   };
 
   home.pointerCursor = {
