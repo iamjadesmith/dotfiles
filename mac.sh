82bc8c334a845823f6f4841a1b134fe3d2ab0ffb
Jade Smith <jade@jade-smith.com>
2025-04-02 07:04:54 -0500

    lsp stuff

--- mac.sh
+++ mac.sh
@@ -5,7 +5,7 @@ xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew install neovim tmux bitwarden-cli syncthing r kubernetes-cli stow helm yt-dlp lazygit zoxide fzf ansible oh-my-posh
+brew install neovim tmux bitwarden-cli syncthing r kubernetes-cli stow helm yt-dlp lazygit zoxide fzf ansible oh-my-posh lua-language-server stylua
 brew install --cask alacritty ente-auth discord alfred obsidian font-jetbrains-mono-nerd-font
 
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
