246c2690bb742b5e02bfb79be12d4b71e5556bc4
Jade Smith <jade@jade-smith.com>
2025-12-23 13:46:55 -0600

    fix treesitter

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -2,26 +2,30 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter").setup({
-				ensure_installed = {
-					"c",
-					"lua",
-					"vim",
-					"vimdoc",
-					"query",
-					"markdown",
-					"markdown_inline",
-					"yaml",
-					"rust",
-					"r",
-					"rnoweb",
-					"python",
-					"bash",
-					"nix",
-				},
-				auto_install = true,
-			})
-		end,
+		lazy = false,
+		branch = "master",
+		main = "nvim-treesitter.configs",
+		opts = {
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"markdown",
+				"markdown_inline",
+				"yaml",
+				"rust",
+				"r",
+				"rnoweb",
+				"python",
+				"bash",
+				"nix",
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+		},
 	},
 }
