6562cba4b3959e25da3c02c8b318cf96c23b5b8b
Jade Smith <jade@jade-smith.com>
2025-12-23 09:41:29 -0600

    fix telescope

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.configs").setup({
+			require("nvim-treesitter").setup({
 				ensure_installed = {
 					"c",
 					"lua",
@@ -20,18 +20,7 @@ return {
 					"bash",
 					"nix",
 				},
-				auto_install = false,
-				highlight = {
-					enable = true,
-					disable = function(lang, buf)
-						local max_filesize = 100 * 1024 -- 100 KB
-						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-						if ok and stats and stats.size > max_filesize then
-							return true
-						end
-					end,
-					additional_vim_regex_highlighting = false,
-				},
+				auto_install = true,
 			})
 		end,
 	},
