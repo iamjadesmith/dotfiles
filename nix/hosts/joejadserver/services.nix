f68a44dae799722bccdd1b188c6568b3635ee759
Jade Smith <jade@jade-smith.com>
2025-12-31 11:11:34 -0600

    freshrss

--- nix/hosts/joejadserver/services.nix
+++ nix/hosts/joejadserver/services.nix
@@ -8,4 +8,10 @@
     backupDir = "/var/local/vaultwarden/backup";
     environmentFile = "/var/lib/vaultwarden/vaultwarden.env";
   };
+
+  services.freshrss = {
+    enable = true;
+    baseUrl = "https://rss.joejad.com";
+    passwordFile = "/run/secrets/freshrss";
+  };
 }
