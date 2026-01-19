50f3fd6ad1c83846378a1140a1d01b58108cfecf
Jade Smith <jade@jade-smith.com>
2026-01-19 16:34:14 -0600

    opencode home-manager

--- nix/hosts/joejadserver/services.nix
+++ nix/hosts/joejadserver/services.nix
@@ -39,4 +39,32 @@
       };
     };
   };
+
+  systemd.services.nextcloud-backup = {
+    description = "Daily Nextcloud data and database backup";
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+    script = ''
+      mkdir -p /mnt/data/mom-backup
+
+      TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+      BACKUP_FILE="/mnt/data/mom-backup/nextcloud_$TIMESTAMP.tar.gz"
+
+      tar -czf "$BACKUP_FILE" -C /mnt/sorserver nextcloud db_backups
+
+      cd /mnt/data/mom-backup
+      ls -t nextcloud_*.tar.gz | tail -n +8 | xargs -r rm --
+    '';
+  };
+
+  systemd.timers.nextcloud-backup = {
+    description = "Daily Nextcloud data and database backup timer";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+  };
 }
