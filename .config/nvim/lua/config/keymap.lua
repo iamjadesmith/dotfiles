33585ad26289a94e7a3e35553cf97cc799506b90
Jade Smith <jade@jade-smith.com>
2026-01-26 08:09:52 -0600

    obsidian sync

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -26,3 +26,4 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 vim.keymap.set("n", "<leader>t", "<cmd>Floaterminal<CR>")
+vim.keymap.set("n", "<leader>os", "<cmd>!obs_sync<CR>", { desc = "Sync Obsidian vault" })
