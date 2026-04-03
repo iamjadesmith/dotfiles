3fe775eec9535f96199d4bbd0f6b5dd2d258677c
Jade Smith <jade@jade-smith.com>
2026-04-03 16:53:16 -0500

    fix warning

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -28,7 +28,7 @@
 
   unstable = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
-      system = final.system;
+      system = final.stdenv.hostPlatform.system;
       config.allowUnfree = true;
     };
   };
