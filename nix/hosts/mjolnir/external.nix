b10fc6448854941686712a9e54a495de48ddce64
Jade Smith <jade@jade-smith.com>
2026-04-28 16:50:13 -0500

    more external

--- nix/hosts/mjolnir/external.nix
+++ nix/hosts/mjolnir/external.nix
@@ -30,5 +30,37 @@
         proxyPass = "https://10.47.59.47:8007";
       };
     };
+
+    "scrypted.joejad.com" = {
+      useACMEHost = "joejad.com";
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "https://10.47.59.11:10443";
+      };
+    };
+
+    "unifi.joejad.com" = {
+      useACMEHost = "joejad.com";
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "https://10.47.59.50:11443";
+      };
+    };
+
+    "ha.joejad.com" = {
+      useACMEHost = "joejad.com";
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://10.47.59.10:8123";
+      };
+    };
+
+    "kvm.joejad.com" = {
+      useACMEHost = "joejad.com";
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://10.47.59.21:80";
+      };
+    };
   };
 }
