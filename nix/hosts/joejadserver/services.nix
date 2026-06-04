726dfe5a9f05f326b958b04fafe7ef84ad0aad35
Jade Smith <jade@jade-smith.com>
2026-06-04 08:09:44 -0500

    paths

--- nix/hosts/joejadserver/services.nix
+++ nix/hosts/joejadserver/services.nix
@@ -5,21 +5,14 @@
   ...
 }:
 
-let
-  githubRunnerWorkDir = "/var/lib/github-runner/joejadserver/work";
-in
 {
   services.github-runners.joejadserver = {
     enable = true;
     name = "joejadserver";
     url = "https://github.com/iamjadesmith/dotfiles";
     tokenFile = config.sops.secrets.github-runner-token.path;
-    workDir = githubRunnerWorkDir;
     serviceOverrides = {
-      BindPaths = lib.mkForce [ ];
-      ExecStartPre = lib.mkBefore [ "${pkgs.coreutils}/bin/mkdir -p ${githubRunnerWorkDir}" ];
-      InaccessiblePaths = lib.mkForce [ "-/run/secrets/github-runner-token" ];
-      WorkingDirectory = lib.mkForce "%S/github-runner/joejadserver";
+      RuntimeDirectoryPreserve = true;
     };
     extraLabels = [
       "joejadserver"
