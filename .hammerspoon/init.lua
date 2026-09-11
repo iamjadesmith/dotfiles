5de77ebbe315e752dce7e7951d0b4ce97d6a9e96
Jade Smith <jade@jade-smith.com>
2026-09-11 16:10:05 -0500

    hammerspoon updates

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,12 +1,16 @@
 local apps = {
-  ["1"] = "Alacritty",
-  ["2"] = "Safari",
-  ["3"] = "Obsidian",
-  ["9"] = "ChatGPT",
+	["1"] = "Alacritty",
+	["2"] = "Safari",
+	["3"] = "Obsidian",
+	["4"] = "Messages",
+	["5"] = "Mail",
+	["6"] = "Reminders",
+	["7"] = "Music",
+	["9"] = "ChatGPT",
 }
 
 for key, app in pairs(apps) do
-  hs.hotkey.bind({ "alt" }, key, function()
-    hs.application.launchOrFocus(app)
-  end)
+	hs.hotkey.bind({ "alt" }, key, function()
+		hs.application.launchOrFocus(app)
+	end)
 end
