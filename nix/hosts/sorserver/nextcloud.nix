6720901dbf07f4ecd85b38734bf718a41c7b4de1
Jade Smith <jade@jade-smith.com>
2026-09-12 10:54:31 -0500

    update nextcloud

--- nix/hosts/sorserver/nextcloud.nix
+++ nix/hosts/sorserver/nextcloud.nix
@@ -15,7 +15,7 @@ in
 {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud33;
+    package = pkgs.nextcloud34;
     hostName = "cloud.${domain}";
     https = true;
     database.createLocally = true;
